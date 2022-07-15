e41fcdca52580534456414e89744e8a4e0a6e93f
SmoothBot <smoothbot123@gmail.com>
2022-07-15 14:52:15 +1000

    Updating GelatoResolver to support tend()/tendTrigger()

--- contracts/GelatoResolver.sol
+++ contracts/GelatoResolver.sol
@@ -5,29 +5,53 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelinupgradeable/contracts/proxy/utils/Initializable.sol";
+import "@openzeppelinupgradeable/contracts/access/OwnableUpgradeable.sol";
 
-interface IKeeperProxy {
-    // Strategy Functions
-    function calcDebtRatio() external view returns (uint256);
-    function calcCollateral() external view returns (uint256);
-    function rebalanceDebt() external;
-    function rebalanceCollateral() external;
-
+interface IKeeperProxyBase {
     // Proxy Keeper Functions
-    function collatTriggerHysteria() external view returns (bool _canExec);
-    function debtTriggerHysteria() external view returns (bool _canExec);
+    function tendTrigger(uint256 _callCost) external view returns (bool _canExec);
+    function tend() external;
+    function harvestTrigger(uint256 _callCost) external view returns (bool _canExec);
+    function harvest() external;
+}
+interface IResolver {
+    // Proxy Keeper Functions
+    function tend() external;
+    function harvest() external;
 }
 
-contract GelatoResolver is Initializable {
+contract GelatoResolver is Initializable, OwnableUpgradeable {
     using SafeMath for uint256;
 
-    function debtTrigger(address keeperProxy) public view returns (bool _canExec, bytes memory _execPayload) {
-        _execPayload = abi.encodeWithSelector(IKeeperProxy(keeperProxy).rebalanceDebt.selector);
-        _canExec = IKeeperProxy(keeperProxy).debtTriggerHysteria();
+    address gelato; // Gelato address
+    
+    function initialize(address _owner, address _gelato) public initializer {
+        _transferOwnership(_owner);
+        gelato = _gelato;
+    }
+
+    modifier onlyGelato() {
+        require(gelato == _msgSender() || owner() == _msgSender(), "GelatoResolver: !gelato");
+        _;
+    }
+
+    function tendTrigger(address keeperProxy) public view returns (bool _canExec, bytes memory _execPayload) {
+        _execPayload = abi.encodeWithSelector(IResolver.tend.selector, keeperProxy);
+        _canExec = IKeeperProxyBase(keeperProxy).tendTrigger(1);
     }
     
-    function collatTrigger(address keeperProxy) public view returns (bool _canExec, bytes memory _execPayload) {
-        _execPayload = abi.encodeWithSelector(IKeeperProxy(keeperProxy).rebalanceCollateral.selector);
-        _canExec = IKeeperProxy(keeperProxy).collatTriggerHysteria();
+    function tend(address keeperProxy) public onlyGelato {
+        IKeeperProxyBase(keeperProxy).tend();
+    }
+
+    function harvestTrigger(address keeperProxy) public view returns (bool _canExec, bytes memory _execPayload) {
+        _execPayload = abi.encodeWithSelector(IResolver.harvest.selector, keeperProxy);
+        _canExec = IKeeperProxyBase(keeperProxy).harvestTrigger(1); // todo: use callcost
+    }
+    
+    function harvest(address keeperProxy) public onlyGelato {
+        // Only trigger if the harvest is needed
+        require(IKeeperProxyBase(keeperProxy).harvestTrigger(1), "!HarvestTigger");
+        IKeeperProxyBase(keeperProxy).harvest(); // todo: use callcost
     }
 }
